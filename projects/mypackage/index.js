8ceb27521c4b8f40cfbe1f530a7542e673c83576
imadityagolu <adityasng420.ak@gmail.com>
2025-05-21 14:35:53 +0530

    updated on 21-5-2025 at 14-35

--- projects/mypackage/index.js
+++ projects/mypackage/index.js
@@ -1,6 +1,16 @@
-const greetings = (username) => {
-    console.log(`Hello ${username}`);
+const readline = require("readline");
+const { stdin, stdout } = require("process");
+
+const greetings = () => { 
+const r1 = readline.createInterface({
+    input: stdin,
+    output: stdout,
+});
+
+r1.question("Type your name?\n", (answer) => {
+    console.log(`\nHello ${answer} welcome to Adityas npm package`);
+    r1.close();
+});
 }
 
-
-MediaSourceHandle.exports = greetings;
+module.exports = greetings;
